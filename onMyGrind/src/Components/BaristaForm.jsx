a76e53236f801d23aeb059e457bc98c73411689b
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-03-05 13:51:24 -0500

    set up form inputs

--- onMyGrind/src/Components/BaristaForm.jsx
+++ onMyGrind/src/Components/BaristaForm.jsx
@@ -1,9 +1,40 @@
 import React, {Component, useState} from "react";
 const BaristaForm = () => {
-  
+    const [inputs, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': ''
+    });
+
+    const ingredients = {
+        'temperature': ['hot', 'lukewarm', 'cold'],
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+        'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'blended': ['yes', 'turbo', 'no']
+    }
+    const onNewDrink = () => {
+
+    }
+    const onCheckAnswer = () => {
+
+    }
   return (
     <div>
+        <h2>Hi, I'd like to order a:</h2>
+        <form>
 
+        </form>
+        <button type="submit" className="button submit" onClick={onCheckAnswer}>
+            Check Answer
+        </button>
+        <button 
+            type="new-drink-button"
+            className="button newdrink"
+            onClick={onNewDrink}
+        >
+            New Drink
+        </button>
     </div>
   );
   
